af92b4588f974d0a2148d0b9359f617ed211161f
ejtang <a123451121@gmail.com>
2024-01-24 19:16:00 +0800

    feat:third commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import "./App.css";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+
+  const [filter, setFilter] = useState("all");
+
   const [todo, setTodo] = useState("");
 
   const handleOnChange = (e) => {
@@ -13,8 +16,14 @@ function App() {
 
   const handleOnSubmit = (e, todoList, todo) => {
     e.preventDefault();
-    setTodoList([...todoList, { id: Date.now(), title: todo, isCompleted: false }]);
-    console.log([...todoList, { id: Date.now(), title: todo, isCompleted: false }]);
+    setTodoList([
+      ...todoList,
+      { id: Date.now(), title: todo, isCompleted: false },
+    ]);
+    console.log([
+      ...todoList,
+      { id: Date.now(), title: todo, isCompleted: false },
+    ]);
     setTodo("");
   };
 
@@ -31,23 +40,39 @@ function App() {
   return (
     <>
       <h1>My todo list</h1>
-      <form onSubmit={(e)=>{handleOnSubmit(e, todoList, todo)}}>
-        <input value={todo} onChange={handleOnChange}/>
+      <button onClick={()=>{setFilter('completed')}}>已完成</button>
+      <button onClick={()=>{setFilter('uncompleted')}}>未完成</button>
+      <button onClick={()=>{setFilter('all')}}>全部</button>
+      <form
+        onSubmit={(e) => {
+          handleOnSubmit(e, todoList, todo);
+        }}
+      >
+        <input value={todo} onChange={handleOnChange} />
       </form>
       <ul>
-      {todoList.map((todo) => {
-        return (
-          <li key={todo.id}>
-            <p>{todo.title}</p>
-            <button
-              onClick={() => {
-                handleOnCompleted(todo.id);
-              }}
-            >
-            {todo.isCompleted ? "已完成" : "未完成"}
-          </button>
-        </li>)
-      })}
+        {todoList.filter((todoFilter)=>{
+          if(filter === "all") {
+            return todoFilter
+          } else if (filter === "completed") {
+            return todoFilter.isCompleted === true
+          } else if (filter === "uncompleted") {
+            return todoFilter.isCompleted === false
+          }
+        }).map((todo) => {
+          return (
+            <li key={todo.id}>
+              <p>{todo.title}</p>
+              <button
+                onClick={() => {
+                  handleOnCompleted(todo.id);
+                }}
+              >
+                {todo.isCompleted ? "已完成" : "未完成"}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
