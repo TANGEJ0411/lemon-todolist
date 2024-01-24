b00b61ac8580196c94d7a5ea28ea6fe7f62f9d80
ejtang <a123451121@gmail.com>
2024-01-24 18:56:23 +0800

    feat:second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
 import { useState } from "react";
 import "./App.css";
-import TodoInput from "./TodoInput";
-import TodoList from "./TodoList";
+// import TodoInput from "./TodoInput";
+// import TodoList from "./TodoList";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const handleOnChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleOnSubmit = (e, todoList, todo) => {
+    e.preventDefault();
+    setTodoList([...todoList, { id: Date.now(), title: todo, isCompleted: false }]);
+    console.log([...todoList, { id: Date.now(), title: todo, isCompleted: false }]);
+    setTodo("");
+  };
+
+  const handleOnCompleted = (id) => {
+    const newTodoList = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: !todo.isCompleted };
+      }
+      return todo;
+    });
+    setTodoList(newTodoList);
+  };
+
   return (
     <>
       <h1>My todo list</h1>
-      <TodoInput todoList={todoList} setTodoList={setTodoList} />
-      <TodoList todoList={todoList} setTodoList={setTodoList} />
+      <form onSubmit={(e)=>{handleOnSubmit(e, todoList, todo)}}>
+        <input value={todo} onChange={handleOnChange}/>
+      </form>
+      <ul>
+      {todoList.map((todo) => {
+        return (
+          <li key={todo.id}>
+            <p>{todo.title}</p>
+            <button
+              onClick={() => {
+                handleOnCompleted(todo.id);
+              }}
+            >
+            {todo.isCompleted ? "已完成" : "未完成"}
+          </button>
+        </li>)
+      })}
+      </ul>
     </>
   );
 }
